abc140803b2371ed8e9646384c6ce9ee5072fb98
Arnold Pham <arnold.pham@hotmail.com>
2024-01-28 09:12:00 +0100

    Modifications pré-présentation

--- main.js
+++ main.js
@@ -14,30 +14,15 @@ function createWindow() {
         icon: path.join(__dirname, '/src/assets/logo_064.png'),
         show: true,
     });
-
     appIcon = new Tray(path.join(__dirname, '/src/assets/logo_064.png'));
-
-    appIcon.on('click', () => {
-        if (win.isVisible()) {
-            win.hide();
-        } else {
-            win.show();
-        }
-    });
-
+    appIcon.on('click', () => { win.isVisible() ? win.hide() : win.show() });
     const contextMenu = Menu.buildFromTemplate([
         {
             label: 'Fermer l\'applications',
-            click: () => {
-                win.destroy();
-            }
+            click: () => { win.destroy() }
         }
     ]);
-
-    appIcon.on('right-click', () => {
-        appIcon.popUpContextMenu(contextMenu);
-    });
-
+    appIcon.on('right-click', () => { appIcon.popUpContextMenu(contextMenu) });
 
     win.setMenu(null);
     win.setMinimumSize(960, 720);
@@ -51,13 +36,9 @@ function createWindow() {
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    if (process.platform !== 'darwin') { app.quit() }
 });
 
 app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
+    if (win === null) { createWindow() }
 });
